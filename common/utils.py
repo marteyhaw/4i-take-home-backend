f46f161944bd592e60a32f8255080513ba4af95a
marteyhaw <martey.haw@gmail.com>
2024-08-24 10:45:56 -0700

    rename common.errors to utils

--- common/utils.py
+++ common/utils.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 
 
-def json_message_response(message: str, status: int) -> JsonResponse:
+def json_message_response(message: str, status: int):
     return JsonResponse(
         {
             "message": message,
