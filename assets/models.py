cf9c6bfd860812a959bc9b6e8e84b6ab5caa1dcc
marteyhaw <martey.haw@gmail.com>
2024-08-24 04:04:21 -0700

    adjust price field of asset model

--- assets/models.py
+++ assets/models.py
@@ -1,10 +1,11 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
 class Asset(models.Model):
     asset_name = models.CharField(max_length=200)
     serial_number = models.CharField(max_length=50, unique=True)
-    price = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     color = models.CharField(max_length=50)
     description = models.TextField(max_length=1000)
     certification = models.BooleanField(default=False)
