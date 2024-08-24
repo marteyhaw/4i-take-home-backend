01f973aa67ff26ead8398277e05fbf6f468d8a9f
marteyhaw <martey.haw@gmail.com>
2024-08-24 01:14:37 -0700

    adjust email and telephone fields of employee model

--- employees/models.py
+++ employees/models.py
@@ -1,17 +1,12 @@
-from django.core.validators import RegexValidator
 from django.db import models
 
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    email = models.EmailField()
-    telephone_regex = RegexValidator(
-        regex=r"^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$",
-        message="Examples: 123-456-7890, (123) 456-7890, 123 456 7890, 123.456.7890, +91 (123) 456-7890",
-    )
-    telephone = models.CharField(validators=[telephone_regex], max_length=17)
-    bio = models.TextField()
+    email = models.EmailField(max_length=200, unique=True)
+    telephone = models.CharField(max_length=17, unique=True)
+    bio = models.TextField(max_length=1000)
     union = models.BooleanField(default=False)
 
     def __str__(self):
