d1725cb90bc0c2d4bf8b6784591bae4cfbd84f6b
marteyhaw <martey.haw@gmail.com>
2024-08-23 23:04:47 -0700

    switch database engine to PostgreSQL

--- th_backend/settings.py
+++ th_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,8 +76,12 @@ WSGI_APPLICATION = "th_backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["PG_DB_NAME"],
+        "USER": os.environ["PG_USERNAME"],
+        "PASSWORD": os.environ["PG_PASSWORD"],
+        "HOST": os.environ["PG_HOSTNAME"],
+        "PORT": os.environ["PG_PORT"],
     }
 }
 
